cf3742c5f4b523277d25a5297932371063c03ef5
sato-kai <h175bkznpicloud.com>
2022-03-08 21:08:28 +0900

    [add]initial commit

---
+++ src/server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 3001;
+app.use(express.static(path.join(__dirname, '../../build')));
+app.get('/', (req, res, next) => res.sendFile(__dirname + './index.html'));
+io.on('connection', function(socket){
+io.emit('message from server', 'message from server - it works!')
+})
+server.listen(port);
